a2bc2d54792a6b044578f42818feccc1c9f3b0f9
bruno <delgado.hebru24@triana.salesianos.edu>
2023-11-15 21:29:49 +0100

    Versión final

--- PruebaDos.java
+++ PruebaDos.java
@@ -2,5 +2,6 @@ public class PruebaDos{
     public static void main (String[] args){
         System.out.println("Segunda prueba");
         System.out.println("BRRR");
+        System.out.println("Me encanta este programa, creo que esta es la versión definitiva");
     }
 }
